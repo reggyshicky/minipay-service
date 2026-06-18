346cf439b79a31a61aea2e169a5e08f168323075
Priscah Reginah Shikanda <reginahshikanda@gmail.com>
2026-06-19 00:06:16 +0300

    initialize mini-pay service repository and initialize the spring boot project using spring initializer

---
+++ minipay-service/src/main/java/com/minipay/minipay_service/MinipayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.minipay.minipay_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinipayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinipayServiceApplication.class, args);
+	}
+
+}
